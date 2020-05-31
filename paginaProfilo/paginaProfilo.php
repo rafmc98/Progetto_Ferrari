01d6f3ef41423b8d9699998947f10aec1238dccb
rafmc98 <65073971+rafmc98@users.noreply.github.com>
2020-05-31 21:19:49 +0200

    profilo

--- paginaProfilo/paginaProfilo.php
+++ paginaProfilo/paginaProfilo.php
@@ -21,7 +21,14 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
-    
+    <script>
+    $(document).ready(function(){
+        $('#iconaProfilo').css({
+            "background-image" : "url('<?php echo $_SESSION['user-pic'] ?>')",
+            "background-size" : "cover"
+            });
+        });
+    </script>
     
     <script>
         $(document).ready(function(){
